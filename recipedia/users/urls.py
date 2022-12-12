cba1811110d7d35fb90d2cd6cd9514e5b3631b9d
Madalina Kopacz <madalina.elena.kopacz@gmail.com>
2022-12-12 12:40:58 +0200

    Add comma

--- recipedia/users/urls.py
+++ recipedia/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [  
-    path('register/', views.create_user_view, name='register'),
-    path('login/', views.login_view, name ='login'),
-    path('logout/', views.logout_view, name ='logout')
-    path('profile/', views.get_user_details, name='profile'),
+urlpatterns = [
+    path("register/", views.create_user_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("profile/", views.get_user_details, name="profile"),
 ]
