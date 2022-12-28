a1437773ff0c58402f7cfdf404b7b0dd17d14d2a
Madalina Kopacz <madalina.elena.kopacz@gmail.com>
2022-12-28 16:54:37 +0200

    Add test heart

--- frontend/recipedia-frontend/src/components/StartPage/RecipeCard.tsx
+++ frontend/recipedia-frontend/src/components/StartPage/RecipeCard.tsx
@@ -1,4 +1,6 @@
 import { Box, Card, Grid, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Recipe } from "../../DTOs";
 
 interface RecipeInfo {
@@ -9,6 +11,25 @@ interface RecipeInfo {
 }
 
 export default function StepCard(props: RecipeInfo) {
+  const userToken = localStorage.getItem("userToken");
+  const [favRecipes, setFavRecipes] = useState<RecipeInfo[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/user/profile/", {
+        headers: {
+          Authorization: userToken,
+        },
+      })
+      .then((resp) => JSON.parse(resp.data.user)[0].fields)
+      .then((userData) => {
+        setFavRecipes(userData?.favorites);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [userToken]);
+
   return (
     <Grid item xs={3} textAlign={"center"} minWidth={450}>
       <Card
@@ -43,22 +64,50 @@ export default function StepCard(props: RecipeInfo) {
         >
           {props.label}
         </Typography>
-        <Typography
+
+        <Grid
+          container
           sx={{
-            fontFamily: "Roboto",
-            fontWeight: 700,
-            color: "#383535",
-            textDecoration: "none",
-            marginX: 4,
-            paddingTop: 2,
-            paddingBottom: 3,
             borderTop: "1px solid #000",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-          variant="h5"
         >
-          {props.dishType && props.dishType[0] ? "Category: " : ""}
-          {props.dishType && props.dishType[0] ? props.dishType[0] : ""}
-        </Typography>
+          <Grid item xs={10}>
+            <Typography
+              sx={{
+                fontFamily: "Roboto",
+                fontWeight: 700,
+                color: "#383535",
+                textDecoration: "none",
+                marginX: 4,
+                paddingTop: 2,
+                paddingBottom: 3,
+              }}
+              variant="h5"
+            >
+              {props.dishType && props.dishType[0] ? "Category: " : ""}
+              {props.dishType && props.dishType[0] ? props.dishType[0] : ""}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={1}
+            sx={{
+              marginRight: 2,
+            }}
+          >
+            {/* {user ? (
+                      <img src="media/favorite-heart.svg" alt="not-favorite-heart" onClick={handleFavoriteOnClick}/>
+                      ))
+                     : (
+                      <Typography sx={{ fontSize: 18, opacity: 0.6 }}>
+                        No favorite recipes.
+                      </Typography>
+                    )} */}
+            <img src="media/favorite-heart-red.svg" alt="not-favorite-heart" />
+          </Grid>
+        </Grid>
       </Card>
     </Grid>
   );
