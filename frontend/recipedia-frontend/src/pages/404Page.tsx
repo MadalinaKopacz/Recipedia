8b37467126e64d586dd858a7fecf6776f79b41e7
Madalina Kopacz <madalina.elena.kopacz@gmail.com>
2023-01-23 02:04:24 +0200

    Fix 404 button

--- frontend/recipedia-frontend/src/pages/404Page.tsx
+++ frontend/recipedia-frontend/src/pages/404Page.tsx
@@ -1,82 +1,91 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Fab, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import ResponsiveAppBar from "../components/Headers/StartPageHeader";
 
-
 const E404Page = () => {
-    return (
-      <>
-        <ResponsiveAppBar/>
-        <Container
-            sx={{
-                marginTop: "5%",
-                width: "50rem",
-                height: "45rem",
-                marginLeft: "auto",
-                marginRight: "auto"
-            }}>
-            <Typography
-            variant="h1"
-            noWrap
-            sx={{
-                ml: 3,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "Playfair",
-                fontWeight: 700,
-                color: "#383535",
-                textDecoration: "none",
-                fontSize: "8rem",
-                marginLeft: "25%",
-                marginBottom: "0.3rem"
-            }} > Sorry 
-            </Typography> 
-            <Typography
-            variant="h4"
-            noWrap
-            sx={{
-                ml: 3,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "Playfair",
-                fontWeight: 700,
-                color: "#383535",
-                textDecoration: "none",
-                marginLeft: "25%"
-            }} > we couldn't find that page
-            </Typography> 
-            <Button
-                href="/"
-                sx={{
-                  my: 2,
-                  display: "block",
-                }}
-                style={{
-                  color: "#DD0426",
-                  backgroundColor: "#FFD9A1",
-                  borderRadius: 20,
-                  width: "25rem",
-                  height: "3.5rem",
-                  fontSize: "24px",
-                  marginLeft: "25%",
-                  textTransform: "none",
-                  fontFamily: "Roboto",
-                  fontWeight: "bold",
-                  marginRight: 30,
-                  padding: "auto",
-                  textAlign: "center",
-                  boxShadow: '1px 1px 5px #383535'
-                }}
-            >Go to the homepage</Button>
-            <img
-            src="media/404img.svg"
-            alt="404 Error"
-            style={{ 
-                width: "35rem", 
-                position: "absolute",
-                top: "20rem",
-                marginLeft: "6.5%"}}
-            />
-        </Container>
-      </>
-    );
+  const navigate = useNavigate();
+  return (
+    <>
+      <ResponsiveAppBar />
+      <Box
+        sx={{
+          marginTop: "5%",
+          width: "50rem",
+          height: "45rem",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <Typography
+          variant="h1"
+          noWrap
+          sx={{
+            ml: 3,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "Playfair",
+            fontWeight: 700,
+            color: "#383535",
+            textDecoration: "none",
+            fontSize: "8rem",
+            marginLeft: "25%",
+            marginBottom: "0.3rem",
+          }}
+        >
+          {" "}
+          Sorry
+        </Typography>
+        <Typography
+          variant="h4"
+          noWrap
+          sx={{
+            ml: 3,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "Playfair",
+            fontWeight: 700,
+            color: "#383535",
+            textDecoration: "none",
+            marginLeft: "25%",
+          }}
+        >
+          {" "}
+          we couldn't find that page
+        </Typography>
+        <Fab
+          sx={{ my: 2, display: "block" }}
+          style={{
+            color: "#DD0426",
+            backgroundColor: "#FFD9A1",
+            borderRadius: 20,
+            width: "25rem",
+            height: "3.5rem",
+            fontSize: "24px",
+            marginLeft: "25%",
+            textTransform: "none",
+            fontFamily: "Roboto",
+            fontWeight: "bold",
+            marginRight: 30,
+            padding: "auto",
+            textAlign: "center",
+            boxShadow: "1px 1px 5px #383535",
+          }}
+          onClick={() => navigate("/")}
+        >
+          Go to the homepage
+        </Fab>
+
+        <img
+          src="media/404img.svg"
+          alt="404 Error"
+          style={{
+            width: "35rem",
+            position: "absolute",
+            top: "20rem",
+            marginLeft: "6.5%",
+          }}
+        />
+      </Box>
+    </>
+  );
 };
 
 export default E404Page;
